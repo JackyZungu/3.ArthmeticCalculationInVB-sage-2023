57d9369e919549ad2ed6854de616d2b395e61c0f
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-03 11:22:48 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        CenterToParent()
+        Me.BackColor = Color.Beige
+
+        MsgBox("2 + 3 = " & 2 + 3 & "  and 2 - 3 = " & 2 - 3,, "Result")
+
+        'MsgBox("2 - 3 = " & 2 - 3,, "Result")
+
+        MsgBox("2 / 3 = " & 2 / 3,, "Result")
+
+        MsgBox("2 * 3 = " & 2 * 3,, "Result")
+    End Sub
+End Class
